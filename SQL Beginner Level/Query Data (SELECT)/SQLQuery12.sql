f9f62e42cbcb6195e15d2e61b357239b51e4ead2
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-09-26 00:22:30 +0500

    Add files via upload

--- SQL Beginner Level/Query Data (SELECT)/SQLQuery12.sql
+++ SQL Beginner Level/Query Data (SELECT)/SQLQuery12.sql
@@ -4,7 +4,7 @@ and return only those countries with an average score greater than 430 */
 
 SELECT
 	country,
-	AVG(score) as average_score
+	AVG(score) as avg_score
 FROM customers
 WHERE score != 0
 GROUP BY country
