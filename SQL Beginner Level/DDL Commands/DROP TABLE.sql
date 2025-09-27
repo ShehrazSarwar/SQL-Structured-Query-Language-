75389c4e063109bba3931a6fcea7bdbb4dae225e
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-09-28 00:06:27 +0500

    Add files via upload

--- SQL Beginner Level/DDL Commands/DROP TABLE.sql
+++ SQL Beginner Level/DDL Commands/DROP TABLE.sql
@@ -1,3 +1,4 @@
 -- DELETE the Table persons from the database
 
-DROP TABLE persons
+DROP TABLE persons -- Will throw error if doesn't exist
+DROP TABLE IF EXISTS persons -- Will not throw error if doesn't exist
