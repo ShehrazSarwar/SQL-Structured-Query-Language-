5d4dc7efb13b836314c7834d5099dc5a6c341765
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-09-30 08:01:06 +0500

    Add files via upload

---
+++ SQL Intermediate Level/INNER JOIN USING LEFT ANTI JOIN.sql
@@ -0,0 +1,9 @@
+-- Alternative to INNER JOIN using LEFT JOIN
+/* Get all customers along with their orders, 
+   but only for customers who have placed an order */
+
+SELECT *
+FROM customers AS c
+LEFT JOIN orders AS o
+ON c.id = o.customer_id
+WHERE o.customer_id IS NOT NULL
