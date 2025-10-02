4effb2606cb54fe0bc2ac17ae13277ec61fe3887
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-10-02 19:12:03 +0500

    Add files via upload

--- SQL Intermediate Level/String Functions/CONCAT.sql
+++ SQL Intermediate Level/String Functions/CONCAT.sql
@@ -1,10 +1,13 @@
 -- CONCAT(Col or val, Col or Val) -> Combine Multiple Strings Into One
 
+-- Concatenate first name and last name into one column FullName
 USE SalesDB
 SELECT
 CONCAT(FirstName,' ',LastName) AS FullName
 FROM sales.Customers
 
+
+-- Concatenate first name and country into one column
 USE MyDatabase
 SELECT
 CONCAT(first_name,'-',country) AS full_info
