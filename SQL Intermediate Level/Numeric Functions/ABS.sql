23b24c1843c9142c143ad8b10ace7cc85335adce
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-10-02 20:34:59 +0500

    Add files via upload

---
+++ SQL Intermediate Level/Numeric Functions/ABS.sql
@@ -0,0 +1,5 @@
+-- ABS(Col or Val)
+
+SELECT 
+	- 110 AS Original_Number,
+	ABS(-110) AS Absolute_Number
---
+++ SQL Intermediate Level/Numeric Functions/ROUND.sql
@@ -0,0 +1,7 @@
+-- ROUND(Col or Val,Round_Value)
+
+SELECT 
+	3.516 AS Original_Number,
+	ROUND(3.516,2) AS Round_2,
+	ROUND(3.516,1) AS Round_1,
+	ROUND(3.516,0) AS Round_0
