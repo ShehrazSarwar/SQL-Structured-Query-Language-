59af44520c6ff1b7e3f5172e74264f554822fb4d
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-10-02 23:56:46 +0500

    Add files via upload

---
+++ SQL Intermediate Level/Date and Time Functions/DATE PARTS USE CASES.sql
@@ -0,0 +1,41 @@
+/* ==============================================================================
+   DATE PARTS | USE CASES
+===============================================================================*/
+
+/* TASK 5:
+   How many orders were placed each year?
+*/
+
+SELECT
+	YEAR(OrderDate) AS Year,
+	COUNT(*) AS OrdersPlaced -- COUNT(*) return no of rows of each group
+FROM Sales.Orders
+GROUP BY YEAR(OrderDate)
+
+/* TASK 6:
+   How many orders were placed each month?
+*/
+
+SELECT 
+	MONTH(OrderDate) AS Month,
+	COUNT(*) AS OrdersPlaced
+FROM Sales.Orders
+GROUP BY MONTH(OrderDate)
+
+/* TASK 7:
+   How many orders were placed each month (using friendly month names)?
+*/
+
+SELECT 
+	DATENAME(Month,(OrderDate)) AS Month,
+	COUNT(*) AS OrdersPlaced
+FROM Sales.Orders
+GROUP BY DATENAME(Month,(OrderDate))
+
+/* TASK 8:
+   Show all orders that were placed during the month of February.
+*/
+
+SELECT * 
+FROM Sales.Orders
+WHERE MONTH(OrderDate) = 2
