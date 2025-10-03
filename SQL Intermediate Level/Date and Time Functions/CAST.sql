9a28bedf9e59fee5fc5f8438486a51962c76f517
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-10-03 17:50:14 +0500

    Add files via upload

--- SQL Intermediate Level/Date and Time Functions/CAST.sql
+++ SQL Intermediate Level/Date and Time Functions/CAST.sql
@@ -1,2 +1,12 @@
 -- CAST(Value AS data_type) 
 -- Convert data types using CAST.
+
+SELECT
+	CAST('123' AS INT) AS [String To Int],
+	CAST(123 AS VARCHAR) AS [Int To String],
+	CAST('2025-10-03' AS DATE) AS [String To Date],
+	CAST('2025-10-03' AS DATETIME2) AS [String To Datetime],
+	CAST(GETDATE() AS VARCHAR) AS [Datetime To String],
+	CreationTime,
+	CAST(CreationTime AS DATE) AS [Datetime To Date]
+FROM Sales.Orders
