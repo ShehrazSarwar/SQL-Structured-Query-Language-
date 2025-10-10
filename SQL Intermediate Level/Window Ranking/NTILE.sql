a427647710f1bdb9045251d66afa6654d159c310
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-10-11 04:15:44 +0500

    Add files via upload

--- SQL Intermediate Level/Window Ranking/NTILE.sql
+++ SQL Intermediate Level/Window Ranking/NTILE.sql
@@ -4,8 +4,10 @@
 SELECT 
     OrderID,
     Sales,
+	ProductID,
     NTILE(1) OVER (ORDER BY Sales) AS OneBucket,
     NTILE(2) OVER (ORDER BY Sales) AS TwoBuckets,
     NTILE(3) OVER (ORDER BY Sales) AS ThreeBuckets,
-    NTILE(4) OVER (ORDER BY Sales) AS FourBuckets
+    NTILE(4) OVER (ORDER BY Sales) AS FourBuckets,
+	NTILE(2) OVER (PARTITION BY ProductID ORDER BY Sales) AS TwoBucketByProducts
 FROM Sales.Orders;
