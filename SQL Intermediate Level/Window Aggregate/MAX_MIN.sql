82085bc55797890d0ba11bfce1c9db04e4730c5f
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-10-11 00:32:24 +0500

    Add files via upload

--- SQL Intermediate Level/Window Aggregate/MAX_MIN.sql
+++ SQL Intermediate Level/Window Aggregate/MAX_MIN.sql
@@ -14,3 +14,21 @@ SELECT
     MAX(Sales) OVER () AS Highest_Sales,
     MIN(Sales) OVER () AS Lowest_Sales
 FROM Sales.Orders
+
+/* TASK 10:
+   Find the Lowest Sales across all orders and by Product
+*/
+
+SELECT 
+    OrderID,
+    ProductID,
+    OrderDate,
+    Sales,
+    MIN(Sales) OVER () AS LowestSales,
+    MIN(Sales) OVER (PARTITION BY ProductID) AS LowestSalesByProduct
+FROM Sales.Orders
+
+/* TASK 11:
+   Show the employees who have the highest salaries
+*/
+
