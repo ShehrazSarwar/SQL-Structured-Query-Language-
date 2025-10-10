ba257e620fd84d66d4c5d0c6908513c8094c8332
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-10-11 04:11:06 +0500

    Add files via upload

--- SQL Intermediate Level/Window Ranking/NTILE.sql
+++ SQL Intermediate Level/Window Ranking/NTILE.sql
@@ -1,3 +1,11 @@
 -- NTILE()
 -- Divide Orders into Groups Based on Sales
 
+SELECT 
+    OrderID,
+    Sales,
+    NTILE(1) OVER (ORDER BY Sales) AS OneBucket,
+    NTILE(2) OVER (ORDER BY Sales) AS TwoBuckets,
+    NTILE(3) OVER (ORDER BY Sales) AS ThreeBuckets,
+    NTILE(4) OVER (ORDER BY Sales) AS FourBuckets
+FROM Sales.Orders;
