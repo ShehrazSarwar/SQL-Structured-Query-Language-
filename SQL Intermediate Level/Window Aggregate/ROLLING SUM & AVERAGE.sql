fbeb855d94df57a76346b4223edda6103de08f7e
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-10-11 00:46:21 +0500

    Add files via upload

---
+++ SQL Intermediate Level/Window Aggregate/ROLLING SUM & AVERAGE.sql
@@ -0,0 +1,7 @@
+/* ============================================================
+   Use Case | ROLLING SUM & AVERAGE
+   ============================================================ */
+
+/* TASK 13:
+   Calculate the moving average of Sales for each Product over time
+*/
