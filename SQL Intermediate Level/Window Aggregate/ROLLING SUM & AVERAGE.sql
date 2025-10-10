e700b0868444a6713747f34473dc07dd27758d39
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-10-11 01:02:59 +0500

    Add files via upload

--- SQL Intermediate Level/Window Aggregate/ROLLING SUM & AVERAGE.sql
+++ SQL Intermediate Level/Window Aggregate/ROLLING SUM & AVERAGE.sql
@@ -5,3 +5,25 @@
 /* TASK 13:
    Calculate the moving average of Sales for each Product over time
 */
+
+SELECT
+	OrderID,
+	ProductID,
+	OrderDate,
+	Sales,
+	AVG(Sales) OVER(PARTITION BY ProductID) AS AvgByProduct,
+	AVG(Sales) OVER(PARTITION BY ProductID ORDER BY OrderDate) AS MovingAvg
+FROM Sales.Orders
+
+/* TASK 14:
+   Calculate the moving average of Sales for each Product over time,
+   including only the next order
+*/
+
+
+SELECT
+	OrderID,
+	ProductID,
+	OrderDate,
+	Sales
+FROM Sales.Orders
