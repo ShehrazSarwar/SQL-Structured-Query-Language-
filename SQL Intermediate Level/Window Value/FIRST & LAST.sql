713d76163bb8ef18f44eb60228b25bcb5d7bf4ae
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-10-12 01:17:14 +0500

    Add files via upload

--- SQL Intermediate Level/Window Value/FIRST & LAST.sql
+++ SQL Intermediate Level/Window Value/FIRST & LAST.sql
@@ -6,3 +6,13 @@
    Find the Lowest and Highest Sales for Each Product,
    and determine the difference between the current Sales and the lowest Sales for each Product.
 */
+
+SELECT
+	OrderID,
+	ProductID,
+	Sales,
+	FIRST_VALUE(Sales) OVER(PARTITION BY ProductID ORDER BY Sales) AS LowestSale,
+	LAST_VALUE(Sales) OVER(PARTITION BY ProductID ORDER BY Sales ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING) AS HighestSale,
+	-- FIRST_VALUE(Sales) OVER(PARTITION BY ProductID ORDER BY Sales DESC) AS HighestSale2,
+	Sales - FIRST_VALUE(Sales) OVER(PARTITION BY ProductID ORDER BY Sales) AS Diff_Between_Lowest
+FROM Sales.Orders
