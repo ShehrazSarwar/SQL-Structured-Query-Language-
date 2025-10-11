bec876cbae417bbf0fa981fbef230a3818dba70f
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-10-11 07:45:50 +0500

    Add files via upload

--- SQL Intermediate Level/Window Ranking/CUME_DIST & PERCENT_RANK.sql
+++ SQL Intermediate Level/Window Ranking/CUME_DIST & PERCENT_RANK.sql
@@ -1,4 +1,4 @@
--- CUME_DIST
+-- CUME_DIST & PERCENT_RANK
 -- Find Products that Fall Within the Highest 40% of the Prices
 
 SELECT 
@@ -10,7 +10,9 @@ FROM (
     SELECT
         Product,
         Price,
-        CUME_DIST() OVER (ORDER BY Price DESC) AS DistRank
+        CUME_DIST() OVER(ORDER BY Price) AS DistRank
     FROM Sales.Products
 ) AS PriceDistribution
-WHERE DistRank <= 0.4;
+WHERE DistRank <= 0.4
+
+-- PERCENT_RANK() --> Use Different Formula For Ranking (exclusive ranking)
