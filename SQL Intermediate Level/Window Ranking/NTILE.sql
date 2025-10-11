3c98791323da92d84c3a26822e7ac4b0b085f4e3
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-10-11 06:03:32 +0500

    Add files via upload

--- SQL Intermediate Level/Window Ranking/NTILE.sql
+++ SQL Intermediate Level/Window Ranking/NTILE.sql
@@ -11,3 +11,24 @@ SELECT
     NTILE(4) OVER (ORDER BY Sales) AS FourBuckets,
 	NTILE(2) OVER (PARTITION BY ProductID ORDER BY Sales) AS TwoBucketByProducts
 FROM Sales.Orders;
+
+
+-- USE CASES:
+-- 1. Segment all Orders into 3 Categories: High, Medium, and Low Sales.
+
+SELECT
+    OrderID,
+    Sales,
+    Buckets,
+    CASE 
+        WHEN Buckets = 1 THEN 'High'
+        WHEN Buckets = 2 THEN 'Medium'
+        WHEN Buckets = 3 THEN 'Low'
+    END AS SalesSegmentations
+FROM (
+    SELECT
+        OrderID,
+        Sales,
+        NTILE(3) OVER (ORDER BY Sales DESC) AS Buckets
+    FROM Sales.Orders
+) AS SalesBuckets;
