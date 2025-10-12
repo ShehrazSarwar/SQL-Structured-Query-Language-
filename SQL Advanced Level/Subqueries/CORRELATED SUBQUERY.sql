c84794e582a0370c99b13978eb32138e2ed7336d
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-10-12 21:31:34 +0500

    Add files via upload

---
+++ SQL Advanced Level/Subqueries/CORRELATED SUBQUERY.sql
@@ -0,0 +1,8 @@
+/* ==============================================================================
+   CORRELATED SUBQUERY
+===============================================================================*/
+
+/* TASK 10:
+   Show all customer details and the total orders for each customer using a correlated subquery.
+*/
+
--- SQL Advanced Level/Subqueries/WHERE - COMPARISON OPERATORS.sql
+++ SQL Advanced Level/Subqueries/WHERE - COMPARISON OPERATORS.sql
@@ -12,3 +12,63 @@ SELECT
 	(SELECT AVG(Price) FROM Sales.Products) AS AvgPrice
 FROM Sales.Products
 WHERE Price > (SELECT AVG(Price) FROM Sales.Products)
+
+
+/* ==============================================================================
+   SUBQUERY | IN OPERATOR
+===============================================================================*/
+
+/* TASK 7:
+   Show the details of orders made by customers in Germany.
+*/
+
+
+SELECT
+    *
+FROM Sales.Orders
+WHERE CustomerID IN (
+    SELECT
+        CustomerID
+    FROM Sales.Customers
+    WHERE Country = 'Germany'
+);
+
+/* TASK 8:
+   Show the details of orders made by customers not in Germany.
+*/
+
+SELECT
+    *
+FROM Sales.Orders
+WHERE CustomerID NOT IN (
+    SELECT
+        CustomerID
+    FROM Sales.Customers
+    WHERE Country = 'Germany'
+);
+
+
+/* ==============================================================================
+   SUBQUERY | ANY OR ALL OPERATOR
+===============================================================================*/
+
+/* TASK 9:
+   Find female employees whose salaries are greater than the salaries of any male employees.
+*/
+
+SELECT
+    EmployeeID, 
+    FirstName,
+    Salary
+FROM Sales.Employees
+WHERE Gender = 'F' AND Salary > ANY(SELECT Salary FROM Sales.Employees WHERE Gender = 'M')
+
+
+/* ==============================================================================
+   SUBQUERY | EXISTS OPERATOR
+===============================================================================*/
+
+/* TASK 11:
+   Show the details of orders made by customers in Germany.
+*/
+
