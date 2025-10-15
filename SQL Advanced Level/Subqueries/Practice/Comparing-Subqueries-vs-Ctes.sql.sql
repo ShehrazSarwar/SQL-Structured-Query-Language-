f14d3482534bb0c83a5ab5ece4bdc28f2bf57306
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-10-16 03:20:50 +0500

    Add files via upload

---
+++ SQL Advanced Level/Subqueries/Practice/Comparing-Subqueries-vs-Ctes.sql.sql
@@ -0,0 +1,88 @@
+-- Comparing SQL Techniques: Subqueries, CTEs, and Window Functions
+
+/* QUESTION: Find stores who's sales where better than the average sales accross all stores */
+
+-- Using Having
+SELECT
+	store_id,
+	store_name,
+	SUM(price) AS TotalSale
+FROM sales
+GROUP BY store_id, store_name
+HAVING SUM(price) > (
+SELECT
+	AVG(TotalSale)
+FROM
+(SELECT
+	store_id,
+	store_name,
+	SUM(price) AS TotalSale
+FROM sales
+GROUP BY store_id, store_name) X )
+
+-- Using JOIN
+SELECT Sales.* 
+FROM (
+SELECT
+	store_id,
+	store_name,
+	SUM(price) AS TotalSales
+FROM sales
+GROUP BY store_id, store_name
+) AS Sales JOIN
+(SELECT
+	AVG(TotalSale) AS AVG_Sale
+FROM
+(SELECT
+	store_id,
+	store_name,
+	SUM(price) AS TotalSale
+FROM sales
+GROUP BY store_id, store_name) X ) AvgSale
+ON Sales.TotalSales > AvgSale.AVG_Sale
+
+-- Using CTE
+WITH CTE_Sales AS (
+		SELECT
+			store_id,
+			store_name,
+			SUM(price) AS TotalSale
+		FROM sales
+		GROUP BY store_id, store_name)
+SELECT * FROM CTE_Sales
+WHERE TotalSale > (SELECT AVG(TotalSale) FROM CTE_Sales);
+
+-- Use Multiple CTE When More Info is Required
+WITH CTE_Sales AS (
+		SELECT
+			store_id,
+			store_name,
+			SUM(price) AS TotalSale
+		FROM sales
+		GROUP BY store_id, store_name
+), 
+CTE_Avg AS (
+	SELECT 
+		AVG(TotalSale) AS AvgSale
+	FROM CTE_Sales)
+
+SELECT *,
+	TotalSale - AvgSale AS DifferInAvg
+FROM CTE_Sales JOIN CTE_Avg
+ON CTE_Sales.TotalSale > CTE_Avg.AvgSale;
+
+
+/* Window functions are great for analytics, like showing the average next to each row. 
+But if I need to filter based on that average like only showing stores above it, 
+I’ll usually wrap it in a CTE or subquery, since window functions can't be used directly in the WHERE clause.
+*/
+
+WITH StoreSales AS (
+	SELECT store_id, store_name, SUM(price) AS TotalSale
+	FROM sales
+	GROUP BY store_id, store_name
+)
+SELECT *,
+	   AVG(TotalSale) OVER () AS AvgSale,
+	   TotalSale - AVG(TotalSale) OVER () AS DiffFromAvg
+FROM StoreSales
---
+++ SQL Advanced Level/Subqueries/Practice/Practice Questions.sql
@@ -0,0 +1,139 @@
+/* QUESTION: Find the employees who's salary is more than the average salary earned by all employees. */
+-- 1) find the avg salary
+-- 2) filter employees based on the above avg salary
+
+SELECT *
+FROM EMPLOYEE
+WHERE SALARY > (SELECT AVG(SALARY) FROM EMPLOYEE)
+ORDER BY SALARY;
+
+/* QUESTION: Find the employees who earn more than the average salary earned by all employees. */
+-- it return exactly 1 row and 1 column
+
+SELECT 
+	E.*,
+	A.Avg_Salary
+FROM EMPLOYEE AS E
+JOIN (SELECT AVG(SALARY) AS Avg_Salary FROM EMPLOYEE) AS A
+ON E.SALARY > A.Avg_Salary
+ORDER BY E.SALARY;
+
+-- Multiple column, multiple row subquery
+/* QUESTION: Find the employees who earn the highest salary in each department. */
+
+-- Using JOIN
+SELECT E.*
+FROM EMPLOYEE AS E
+JOIN 
+(SELECT
+	DEPT_NAME,
+	MAX(SALARY) AS SALARY
+FROM EMPLOYEE
+GROUP BY DEPT_NAME) AS EE
+ON E.DEPT_NAME = EE.DEPT_NAME
+AND E.SALARY = EE.SALARY;
+
+-- Using RANK() WINDOW 
+SELECT *
+FROM
+(SELECT
+	*,
+	RANK() OVER(PARTITION BY DEPT_NAME ORDER BY SALARY DESC) AS RN
+FROM EMPLOYEE) AS X
+WHERE RN = 1;
+
+-- Using Correlated Subquery
+SELECT *
+FROM EMPLOYEE AS E
+WHERE SALARY = (
+			SELECT
+				MAX(SALARY)
+			FROM EMPLOYEE
+			WHERE E.DEPT_NAME = DEPT_NAME
+);
+
+-- Single column, multiple row subquery
+/* QUESTION: Find department who do not have any employees */
+
+SELECT *
+FROM department
+WHERE dept_name NOT IN (SELECT DISTINCT dept_name FROM EMPLOYEE);
+
+--------------------------------------------------------------------------------
+/* < CORRELATED SUBQUERY >
+-- A subquery which is related to the Outer query
+QUESTION: Find the employees in each department who earn more than the average salary in that department.
+1) find the avg salary per department
+2) filter data from employee tables based on avg salary from above result. */
+
+SELECT *
+FROM EMPLOYEE E
+WHERE SALARY > (
+SELECT
+	AVG(SALARY)
+FROM EMPLOYEE EE
+WHERE EE.DEPT_NAME = E.DEPT_NAME)
+ORDER BY E.DEPT_NAME, E.SALARY;
+
+
+/* QUESTION: Find department who do not have any employees */
+-- Using correlated subquery
+
+SELECT *
+FROM department D
+WHERE NOT EXISTS (SELECT 1 FROM EMPLOYEE E WHERE D.DEPT_NAME = E.dept_name);
+
+
+--------------------------------------------------------------------------------
+/* < SUBQUERY inside SUBQUERY (NESTED Query/Subquery)> */
+/* QUESTION: Find stores who's sales where better than the average sales accross all stores */
+
+-- Using Having
+SELECT
+	store_id,
+	store_name,
+	SUM(price) AS TotalSale
+FROM sales
+GROUP BY store_id, store_name
+HAVING SUM(price) > (
+SELECT
+	AVG(TotalSale)
+FROM
+(SELECT
+	store_id,
+	store_name,
+	SUM(price) AS TotalSale
+FROM sales
+GROUP BY store_id, store_name) X )
+
+-- Using JOIN
+SELECT Sales.* 
+FROM (
+SELECT
+	store_id,
+	store_name,
+	SUM(price) AS TotalSales
+FROM sales
+GROUP BY store_id, store_name
+) AS Sales JOIN
+(SELECT
+	AVG(TotalSale) AS AVG_Sale
+FROM
+(SELECT
+	store_id,
+	store_name,
+	SUM(price) AS TotalSale
+FROM sales
+GROUP BY store_id, store_name) X ) AvgSale
+ON Sales.TotalSales > AvgSale.AVG_Sale
+
+-- Using CTE
+WITH CTE_Sales AS (
+		SELECT
+			store_id,
+			store_name,
+			SUM(price) AS TotalSale
+		FROM sales
+		GROUP BY store_id, store_name)
+SELECT * FROM CTE_Sales
+WHERE TotalSale > (SELECT AVG(TotalSale) FROM CTE_Sales);
