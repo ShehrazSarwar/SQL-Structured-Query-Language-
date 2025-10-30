d88d97e9041215e5e8dbf60ad924e31eb3f54259
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-10-31 02:22:03 +0500

    Updated

--- SQL Advanced Level/Stored Procedure/Multiple Queries in Stored Procedure.sql
+++ SQL Advanced Level/Stored Procedure/Multiple Queries in Stored Procedure.sql
@@ -2,3 +2,24 @@
    Multiple Queries in Stored Procedure
 ============================================================================== */
 
+-- Update the Stored Procedure
+ALTER PROCEDURE GetCustomerSummary @country NVARCHAR(50) = 'USA' AS
+BEGIN
+	SELECT
+        COUNT(*) AS TotalCustomers,
+        AVG(Score) AS AvgScore
+    FROM Sales.Customers
+    WHERE Country = @country
+
+	SELECT
+		COUNT(*) AS TotalOrders,
+		SUM(Sales) AS TotalSales
+	FROM Sales.Customers AS c
+	JOIN Sales.Orders AS o
+	ON c.CustomerID = o.CustomerID
+	WHERE c.Country = @country
+END
+
+--Execute Stored Procedure
+EXEC GetCustomerSummary @country = 'Germany'
+EXEC GetCustomerSummary
