8a86be27c3f27de8d3f8fba3fd44040eee25a001
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-10-31 00:36:09 +0500

    Add files via upload

---
+++ SQL Advanced Level/CTAS and Temp Tables/Temp_Tables.sql
@@ -0,0 +1,26 @@
+-- Temp Tables
+/* ==============================================================================
+   Step 1: Create Temporary Table (#Orders)
+============================================================================== */
+SELECT 
+	*
+INTO #Orders
+FROM Sales.Orders;
+
+
+/* ==============================================================================
+   Step 2: Clean Data in Temporary Table
+============================================================================== */
+DELETE FROM #Orders
+WHERE OrderStatus = 'Delivered';
+
+
+/* ==============================================================================
+   Step 3: Load Cleaned Data into Permanent Table (Sales.OrdersTest)
+============================================================================== */
+SELECT 
+	*
+INTO Sales.OrdersTest
+FROM #Orders
+
+SELECT * FROM Sales.OrdersTest
