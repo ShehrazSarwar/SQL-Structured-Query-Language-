7ca00f291daabfea11b87d19a62efea2b7bd96c7
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-11-01 01:41:31 +0500

    Add files via upload

---
+++ SQL Advanced Level/Triggers/TRIGGER.sql
@@ -0,0 +1,30 @@
+-- TRIGGER IN SQL - With Log Use Case
+
+-- Create a Table For Trigger
+CREATE TABLE Sales.EmployeeLogs (
+	LogID INT IDENTITY(1,1) PRIMARY KEY,
+	EmployeeID INT,
+	LogMessage VARCHAR(255),
+	LogDate DATE
+)
+
+-- Creating Trigger
+CREATE TRIGGER trg_AfterInsertEmployee ON Sales.Employees
+AFTER INSERT
+AS
+BEGIN
+	INSERT INTO Sales.EmployeeLogs (EmployeeID, LogMessage, LogDate)
+	SELECT
+		EmployeeID,
+		'New Employee Added = ' + CAST(EmployeeID AS VARCHAR),
+		GETDATE()
+	FROM INSERTED
+END
+
+
+-- Checking Trigger By Inserting New Employees
+INSERT INTO Sales.Employees
+VALUES (7, 'Zainab', 'Idrees', 'IT', '2005-01-15', 'F', 80000, 3)
+
+-- Check If Trigger Works
+SELECT * FROM Sales.EmployeeLogs
