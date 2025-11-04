2e138203899363fbdb86ad4bb4ecced3a44dbcd0
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-11-04 22:13:47 +0500

    Add files via upload

---
+++ SQL Advanced Level/Indexes/Unique Indexes.sql
@@ -0,0 +1,17 @@
+/* ==============================================================================
+   Unique Indexes
+============================================================================== */
+
+-- Attempt to create a Unique Index on the Category column in Sales.Products.
+-- Note: This may fail if duplicate values exist.
+
+CREATE UNIQUE INDEX idx_Products_Category
+ON Sales.Products (Category);
+  
+-- Create a Unique Index on the Product column in Sales.Products
+CREATE UNIQUE INDEX idx_Products_Product
+ON Sales.Products (Product);
+  
+-- Test Insert: Attempt to insert a duplicate value (should fail if the constraint is enforced)
+INSERT INTO Sales.Products (ProductID, Product)
+VALUES (106, 'Caps');
