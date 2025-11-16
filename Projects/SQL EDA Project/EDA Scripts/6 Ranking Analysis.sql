ffecf03cd0d8e743b1d6fba7b27e23704aebe591
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-11-16 23:59:31 +0500

    Add files via upload

---
+++ Projects/SQL EDA Project/EDA Scripts/6 Ranking Analysis.sql
@@ -0,0 +1,14 @@
+-- Which 5 products Generating the Highest Revenue?
+-- Simple Ranking
+
+
+-- Complex but Flexibly Ranking Using Window Functions
+
+
+-- What are the 5 worst-performing products in terms of sales?
+
+
+-- Find the top 10 customers who have generated the highest revenue
+
+
+-- The 3 customers with the fewest orders placed
